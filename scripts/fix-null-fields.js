3987463a899e5bba5d02c68584e41eaa80d60f7f
Oladipupo Rancho <oladipuporancho10@gmail.com>
2025-05-08 20:29:57 +0100

    got

---
+++ scripts/fix-null-fields.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+async function fixNullFields() {
+    await prisma.user.updateMany({
+        where: { bvn: null },
+        data: { bvn: 'not_set' },
+    });
+    await prisma.user.updateMany({
+        where: { bankAccount: null },
+        data: { bankAccount: 'not_set' },
+    });
+    console.log("✅ NULL fields fixed.");
+}
+fixNullFields()
+    .catch((e) => {
+    console.error(e);
+    process.exit(1);
+})
+    .finally(() => prisma.$disconnect());
