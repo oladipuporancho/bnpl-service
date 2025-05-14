bb6335c9553a96820e68d087eb88564e733810e1
Oladipupo Rancho <oladipuporancho10@gmail.com>
2025-05-14 02:51:19 +0100

    Enable CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for all origins (not safe for production)
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: false, 
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
